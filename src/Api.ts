e236a2cc76bed32bd2ca802d9b676985ffcde56e
velkorra <anton.s.vertunov@gmail.com>
2025-01-13 20:40:29 +0300

    get api url from env

--- src/Api.ts
+++ src/Api.ts
@@ -10,7 +10,7 @@ import {
 import { Master } from "./models/master";
 import { NoblePhantasm } from "./models/NoblePhantasm";
 
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000/";
 
 export const getServants = async (): Promise<Servant[]> => {
   try {
